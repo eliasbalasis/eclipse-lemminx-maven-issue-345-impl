8a9c47156df784827078e0ee2a6c388c2022e30a
Balasis, Ilias <ilias.balasis@nielseniq.com>
2024-01-22 14:37:44 +0000

    versions

--- modules/app-core/src/main/java/net/upperlimit/impl/app/core/ApplicationProperties.java
+++ modules/app-core/src/main/java/net/upperlimit/impl/app/core/ApplicationProperties.java
@@ -1,6 +1,10 @@
 package net.upperlimit.impl.app.core;
 
+import net.upperlimit.root.support.DomainConstants;
+
 public final class ApplicationProperties {
+
+	public static final String DOMAIN = DomainConstants.DOMAIN_NAME;
 
 	public static final String PROPERTY_1 = "property1";
 
